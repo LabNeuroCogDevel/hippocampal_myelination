df9c0068720f97a8c0368720b9ff0eeaf7f50e3b
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2026-01-28 16:32:56 -0500

    hippunfold flat surface AP LR axes

--- image_processing/7T_MP2RAGE/hippunfold_R1/hippunfold_anatomical_axes.R
+++ image_processing/7T_MP2RAGE/hippunfold_R1/hippunfold_anatomical_axes.R
@@ -13,8 +13,8 @@ extract_coords <- function(hemi, hemi.name, surf){
 	surface_gifti <- readgii(surface_file)
 	AP.coords <- scale(surface_gifti$data$pointset[,1]) %>% as.vector() #A-P coordinates z-score
 	LR.coords <- scale(surface_gifti$data$pointset[,2]) %>% as.vector() #L-R coordinates z-score
-	write_metric_gifti(x = AP.coords, gifti_fname = sprintf("%s/output_measures/7T_MP2RAGE/hippunfold_R1/hippunfold_hemi-%s_label-%s_stat-APaxis.shape.gii", project_dir, hemi.name, surf), hemisphere = hemi)
-	write_metric_gifti(x = LR.coords, gifti_fname = sprintf("%s/output_measures/7T_MP2RAGE/hippunfold_R1/hippunfold_hemi-%s_label-%s_stat-LRaxis.shape.gii", project_dir, hemi.name, surf), hemisphere = hemi)
+	write_metric_gifti(x = AP.coords, gifti_fname = sprintf("%s/output_measures/7T_MP2RAGE/hippunfold_R1/hippunfold_hemi-%s_label-%s_stat-APaxis.shape.gii", project_dir, hemi.name, surf), hemisphere = hemi, col_names = c("axis"))
+	write_metric_gifti(x = LR.coords, gifti_fname = sprintf("%s/output_measures/7T_MP2RAGE/hippunfold_R1/hippunfold_hemi-%s_label-%s_stat-LRaxis.shape.gii", project_dir, hemi.name, surf), hemisphere = hemi, col_names = c("axis"))
 }
 
 ########### Main hippocampal surface (label-hipp) ###########
